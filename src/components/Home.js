e6b5fa5cc8fbfdec8de5a6a4e6ddb237ca1a6fb7
ting-an-chen <ting-an-chen@users.noreply.github.com>
2020-06-09 02:10:33 +0800

    copy photos

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import '../css/main.css';
 
 class Home extends Component {
   render() {
     return (
-        <div>
-          <h2>Home</h2>
-        </div>
+  <div class="main">
+    <div id="page_container">
+      <img id="homepic" src="../photography/IMG_9327.JPG"></img>
+      <div class="maintext">
+        <span1>Welcome to An's personal website!</span1>
+        <p>This is where I record my life stories, photographs I took, and crafts I made.</p>
+      </div>
+    </div>
+  </div>
     );
   }
 }
