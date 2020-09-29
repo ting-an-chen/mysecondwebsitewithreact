4a70b7ac995e868174ae2e04c96671022bfc253f
vs-slavchev <vs_slavchev2@abv.bg>
2020-09-29 23:19:41 +0200

    Add blog post template

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import BlogPost from './BlogPost.js';
-import { areYouBraveEnoughIntro, areYouBraveEnoughContent } from './blog-post-contents/are-you-brave-enough.js';
+import {storyLungCancerPatientIntro, storyLungCancerPatientContent} from './blog-post-contents/story-lung-cancer-patient.js';
+import {areYouBraveEnoughIntro, areYouBraveEnoughContent} from './blog-post-contents/are-you-brave-enough.js';
 import '../css/main.css';
 import '../css/blog.css';
 import '../css/readable-content.css';
@@ -10,6 +11,7 @@ class Blog extends Component {
   render() {
     return (
         <div className="main" id="page_container">
+          <BlogPost title="A Story about a Lung Cancer Patient." date="November 3, 2019" intro={storyLungCancerPatientIntro} content={storyLungCancerPatientContent}/>
           <BlogPost title="Are You Brave Enough to Say Goodbye?" date="October 21, 2019" intro={areYouBraveEnoughIntro} content={areYouBraveEnoughContent}/>
         </div>
     );
---
+++ src/components/blog-post-contents/post-template.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const postIntro = (<React.Fragment>
+            <p> example intro </p>
+</React.Fragment>);
+
+const postContent = (<React.Fragment>
+            <p>content paragraph 1</p>
+            <p>content paragraph 2</p>
+</React.Fragment>);
+
+export {postIntro, postContent};
