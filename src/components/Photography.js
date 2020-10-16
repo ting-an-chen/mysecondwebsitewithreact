39de83ababe05f46729c4f37fa318cb787d9a991
Veselin Slavchev <veselin.slavchev@ti8m.ch>
2020-10-16 17:58:17 +0200

    Extract albums loading

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -1,54 +1,20 @@
 import React, { Component } from 'react';
 
 import Album from './photography/Album.js';
-import image1 from '../photography/ALBUM_1/IMG_1.JPG';
-import image2 from '../photography/ALBUM_1/IMG_2.JPG';
-import image3 from '../photography/ALBUM_1/IMG_3.JPG';
-import image4 from '../photography/ALBUM_1/IMG_4.JPG';
-import image5 from '../photography/ALBUM_1/IMG_5.JPG';
-import image6 from '../photography/ALBUM_1/IMG_6.JPG';
-import image7 from '../photography/ALBUM_1/IMG_7.JPG';
-import image8 from '../photography/ALBUM_1/IMG_8.JPG';
-import image9 from '../photography/ALBUM_1/IMG_9.JPG';
-import image10 from '../photography/ALBUM_1/IMG_10.JPG';
-import image11 from '../photography/ALBUM_1/IMG_11.JPG';
-import image12 from '../photography/ALBUM_1/IMG_12.JPG';
-import image13 from '../photography/ALBUM_1/IMG_13.JPG';
-import image14 from '../photography/ALBUM_1/IMG_14.JPG';
-import image15 from '../photography/ALBUM_1/IMG_15.JPG';
-import image16 from '../photography/ALBUM_1/IMG_16.JPG';
-import image17 from '../photography/ALBUM_1/IMG_17.JPG';
-import image18 from '../photography/ALBUM_1/IMG_18.JPG';
-import image19 from '../photography/ALBUM_1/IMG_19.JPG';
-import image20 from '../photography/ALBUM_1/IMG_20.JPG';
-import image21 from '../photography/ALBUM_1/IMG_21.JPG';
+import {album1, album2 } from './photography/AlbumsLoader.js';
+
 import '../css/modal-image.css';
 import '../css/image-gallery.css';
 
 class Photography extends Component {
-
-  album1 = [
-    {src: image1, description: "descqqq 1"},
-    {src: image2, description: "descqqq 1"},
-    {src: image3, description: "descqqq 1"},
-    {src: image4, description: "descqqq 1"},
-    {src: image5, description: "descqqq 1"},
-    {src: image6, description: "descqqq 1"},
-    {src: image7, description: "descqqq 1"},
-    {src: image8, description: "descqqq 1"},
-    {src: image9, description: "descqqq 1"},
-    {src: image10, description: "descqqq 1"},
-    {src: image11, description: "descqqq 1"},
-    {src: image12, description: "descqqq 1"},
-    {src: image13, description: "descqqq 2"}
-  ];
 
   render() {
     return (
       <div>
         <h2>Photography</h2>
 
-        <Album albumTitle="first album" images={this.album1} />
+        <Album albumTitle="first album" images={album1} />
+        <Album albumTitle="second album" images={album2} />
 
       </div>
     );
---
+++ src/components/photography/AlbumsLoader.js
@@ -0,0 +1,83 @@
+import album1_image1 from '../../photography/ALBUM_1/IMG_1.JPG';
+import album1_image2 from '../../photography/ALBUM_1/IMG_2.JPG';
+import album1_image3 from '../../photography/ALBUM_1/IMG_3.JPG';
+import album1_image4 from '../../photography/ALBUM_1/IMG_4.JPG';
+import album1_image5 from '../../photography/ALBUM_1/IMG_5.JPG';
+import album1_image6 from '../../photography/ALBUM_1/IMG_6.JPG';
+import album1_image7 from '../../photography/ALBUM_1/IMG_7.JPG';
+import album1_image8 from '../../photography/ALBUM_1/IMG_8.JPG';
+import album1_image9 from '../../photography/ALBUM_1/IMG_9.JPG';
+import album1_image10 from '../../photography/ALBUM_1/IMG_10.JPG';
+import album1_image11 from '../../photography/ALBUM_1/IMG_11.JPG';
+import album1_image12 from '../../photography/ALBUM_1/IMG_12.JPG';
+import album1_image13 from '../../photography/ALBUM_1/IMG_13.JPG';
+import album1_image14 from '../../photography/ALBUM_1/IMG_14.JPG';
+import album1_image15 from '../../photography/ALBUM_1/IMG_15.JPG';
+import album1_image16 from '../../photography/ALBUM_1/IMG_16.JPG';
+import album1_image17 from '../../photography/ALBUM_1/IMG_17.JPG';
+import album1_image18 from '../../photography/ALBUM_1/IMG_18.JPG';
+import album1_image19 from '../../photography/ALBUM_1/IMG_19.JPG';
+import album1_image20 from '../../photography/ALBUM_1/IMG_20.JPG';
+import album1_image21 from '../../photography/ALBUM_1/IMG_21.JPG';
+
+import album2_image1 from '../../photography/ALBUM_2/IMG_1.JPG';
+import album2_image2 from '../../photography/ALBUM_2/IMG_2.JPG';
+import album2_image3 from '../../photography/ALBUM_2/IMG_3.JPG';
+import album2_image4 from '../../photography/ALBUM_2/IMG_4.JPG';
+import album2_image5 from '../../photography/ALBUM_2/IMG_5.JPG';
+import album2_image6 from '../../photography/ALBUM_2/IMG_6.JPG';
+import album2_image7 from '../../photography/ALBUM_2/IMG_7.JPG';
+import album2_image8 from '../../photography/ALBUM_2/IMG_8.JPG';
+import album2_image9 from '../../photography/ALBUM_2/IMG_9.JPG';
+import album2_image10 from '../../photography/ALBUM_2/IMG_10.JPG';
+import album2_image11 from '../../photography/ALBUM_2/IMG_11.JPG';
+import album2_image12 from '../../photography/ALBUM_2/IMG_12.JPG';
+import album2_image13 from '../../photography/ALBUM_2/IMG_13.JPG';
+import album2_image14 from '../../photography/ALBUM_2/IMG_14.JPG';
+import album2_image15 from '../../photography/ALBUM_2/IMG_15.JPG';
+import album2_image16 from '../../photography/ALBUM_2/IMG_16.JPG';
+
+const album1 = [
+    { src: album1_image1, description: "example description" },
+    { src: album1_image2, description: "example description" },
+    { src: album1_image3, description: "example description" },
+    { src: album1_image4, description: "example description" },
+    { src: album1_image5, description: "example description" },
+    { src: album1_image6, description: "example description" },
+    { src: album1_image7, description: "example description" },
+    { src: album1_image8, description: "example description" },
+    { src: album1_image9, description: "example description" },
+    { src: album1_image10, description: "example description" },
+    { src: album1_image11, description: "example description" },
+    { src: album1_image12, description: "example description" },
+    { src: album1_image13, description: "example description" },
+    { src: album1_image14, description: "example description" },
+    { src: album1_image15, description: "example description" },
+    { src: album1_image16, description: "example description" },
+    { src: album1_image17, description: "example description" },
+    { src: album1_image18, description: "example description" },
+    { src: album1_image19, description: "example description" },
+    { src: album1_image20, description: "example description" },
+    { src: album1_image21, description: "example description" }
+];
+
+const album2 = [
+    { src: album2_image1, description: "example description" },
+    { src: album2_image2, description: "example description" },
+    { src: album2_image3, description: "example description" },
+    { src: album2_image4, description: "example description" },
+    { src: album2_image5, description: "example description" },
+    { src: album2_image6, description: "example description" },
+    { src: album2_image7, description: "example description" },
+    { src: album2_image8, description: "example description" },
+    { src: album2_image9, description: "example description" },
+    { src: album2_image10, description: "example description" },
+    { src: album2_image11, description: "example description" },
+    { src: album2_image12, description: "example description" },
+    { src: album2_image13, description: "example description" },
+    { src: album2_image14, description: "example description" },
+    { src: album2_image15, description: "example description" },
+    { src: album2_image16, description: "example description" }
+];
+
+export { album1, album2 };
