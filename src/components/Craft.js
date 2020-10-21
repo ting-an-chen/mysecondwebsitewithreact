72b500b453df29b1c26a2d926a11c73ce8f07e5c
vs-slavchev <vs_slavchev2@abv.bg>
2020-10-21 23:08:06 +0200

    Add crafts page

--- src/components/Craft.js
+++ src/components/Craft.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 
+import '../css/craft.css';
+
+import image_cutboard from '../photography/profoliopics/IMG_0095.JPG';
+import image_babies from '../photography/profoliopics/IMG_0141.JPG';
+import image_space from '../photography/profoliopics/IMG_4045.JPG';
+import image_snowman from '../photography/profoliopics/IMG_2013.JPG';
+import image_plushy from '../photography/profoliopics/IMG_9817.JPG';
+
 class Craft extends Component {
+
+  renderImage(mainText, date, materials, imgSrc) {
+    return (
+      <div className="hvrbox2">
+        <img src={imgSrc} className="project" alt="" />
+        <div class="word_container">
+          <div class="description">
+              <h3>{mainText}</h3>
+              <span1>Information:</span1>
+              <span1>Date: {date}</span1>
+              <span1>Materials: {materials}</span1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     return (
-        <div>
-          <h2>Craft</h2>
+      <div>
+        <h2>Craft</h2>
+
+        <div className="picture_container">
+          {this.renderImage("Doctors?", "July 2016", "cutboard, marker", image_cutboard)}
+          {this.renderImage("Babies?", "July 2016", "cutboard, marker", image_babies)}
+          {this.renderImage("Space!", "July 2016", "cutboard, marker", image_space)}
+          {this.renderImage("Snowman?", "July 2016", "cutboard, marker", image_snowman)}
+          {this.renderImage("Pig plushy", "July 2016", "cutboard, marker", image_plushy)}
         </div>
+      </div>
     );
   }
 }
